7c9c9604e03fd22a6569c4d1ba74860fe0652577
hereiznoy <hereiznoy@gmail.com>
2012-07-11 11:25:30 -0500

    Implement backup command

--- snipt-client.js
+++ snipt-client.js
@@ -1,17 +1,66 @@
-var https = require('https');
-
-var httpsOptions = { };
+var https = require('https'),
+	fs = require('fs'),
+	path = require('path');
 
 var SniptClient = function(username, apiKey) {
-	httpsOptions.host = 'snipt.net';
-	httpsOptions.path = '/api/private/snipt/?username=' + username + '&api_key=' + apiKey + '&format=json';
-};
-
-SniptClient.prototype.backup = function(backupDir) {
-	console.log('backup dir = %s', backupDir);
-	//var req = https.get(httpsOptions, function (res) {
-
-	//};
+	this.username = username;
+	this.apiKey = apiKey;
+	this.backup = backup;
 };
 
 module.exports.SniptClient = SniptClient;
+
+var backup = function(backupDir, reqPath) {
+	var options = {
+		host: 'snipt.net',
+		path: reqPath || '/api/private/snipt/?username=' + this.username + '&api_key=' + this.apiKey + '&format=json'
+	};
+	get(options,
+		function (result) {
+			var json = JSON.parse(result);
+			var snipts = json.objects;
+			var meta = json.meta;
+			for (var i = 0; i < snipts.length; i++) {
+				saveSniptToFile(snipts[i], backupDir,
+					function (filePath) {
+						console.log('Saved: %s', filePath);
+					},
+					function (filePath, err) {
+						console.log('There was an error saving: %s', filePath);
+						console.log(err);
+					});
+			}
+
+			// recurse until all snipt's have been pulled down & saved
+			if (meta.next !== null) backup(backupDir, meta.next);
+		},
+		function (err) {
+			console.log('There was an error getting your snipts from Snipt.net');
+			console.log(err);
+		});
+};
+
+var get = function(options, onSuccess, onErr) {
+	var result = '';
+	var req = https.get(options, function (res) {
+		res.setEncoding('utf8');
+		res.on('data', function (chunk) {
+			result += chunk;
+		});
+		res.on('end', function () {
+			onSuccess(result);
+		});
+	}).on('error', function (err) {
+		onErr(err);
+	}).end();
+}
+
+var saveSniptToFile = function (snipt, saveDir, onSuccess, onErr) {
+	var title = snipt.title;
+	var filePath = path.join(saveDir, title + '.txt');
+
+	fs.writeFile(filePath, snipt.code, function (err) {
+		if (err) onErr(filePath, err);
+		else onSuccess(filePath);
+	});
+};
